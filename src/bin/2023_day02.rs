3e17861289b82caebefa279e2b0d51d57e070d84
Nicolas Legrand <nicolas.legrand@gmail.com>
2023-12-02 14:08:46 +0100

    Day 2 template

---
+++ src/bin/2023_day02.rs
@@ -0,0 +1,55 @@
+use std::fs;
+
+// https://adventofcode.com/2023/day/2
+
+fn puzzle1(contents: String) -> u32 {
+    for line in contents.split("\n") {
+    }
+    0
+}
+
+fn puzzle2(contents: String) -> u32 {
+    for line in contents.split("\n") {
+
+    }
+    0
+}
+
+
+fn main() {
+    let file_path = "inputs/2023/day02";
+    let contents = fs::read_to_string(file_path)
+        .expect("No input yet");
+    let res1 = puzzle1(contents.clone());
+    println!("Puzzle 1: {}", res1);
+    let res2 = puzzle2(contents.clone());
+    println!("Puzzle 2: {}", res2);
+    
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    const EXAMPLE_INPUT_1: &str = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+
+    const EXAMPLE_INPUT_2: &str = "";
+
+
+    #[test]
+    fn puzzle1_example() {
+
+        assert_eq!(puzzle1(EXAMPLE_INPUT_1.to_string()), 8);
+    }
+
+    #[test]
+    fn puzzle2_example() {
+
+        assert_eq!(puzzle2(EXAMPLE_INPUT_2.to_string()), 0);
+    }
+
+}
